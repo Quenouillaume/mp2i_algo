deb762036bfcd04541f02973172a30f1c94c483a
Guillaume <glm952@gmail.com>
2023-07-01 00:03:58 +0200

    added some algos (to implement)

--- navigation.php
+++ navigation.php
@@ -10,6 +10,8 @@
 
 <li> <a href="/guillaume.rousseau/algo/tableau/insert_sort">Tri insertion</a></li>
 
+<li> <a href="/guillaume.rousseau/algo/tableau/merge_sort">Tri fusion</a></li>
+
 </ul>
 </div>
 
@@ -17,6 +19,15 @@
 <div class=category>
 <h3><a href="/guillaume.rousseau/algo/graphe">Graphes</a></h3>
 <ul>
+
+<li> <a href="/guillaume.rousseau/algo/graphe/dfs">Parcours en profondeur</a></li>
+
+<li> <a href="/guillaume.rousseau/algo/graphe/bfs">Parcours en largeur</a></li>
+
+<li> <a href="/guillaume.rousseau/algo/graphe/dijkstra">Dijkstra</a></li>
+
+<li> <a href="/guillaume.rousseau/algo/graphe/floyd_warshall">Floyd-Warshall</a></li>
+
 
 </ul>
 </div>
@@ -26,8 +37,27 @@
 <h3><a href="/guillaume.rousseau/algo/texte">Textes</a></h3>
 <ul>
 
+<li> <a href="/guillaume.rousseau/algo/texte/lzw">LZW</a></li>
+
+<li> <a href="/guillaume.rousseau/algo/texte/huffman">Huffman</a></li>
+
+<li> <a href="/guillaume.rousseau/algo/texte/rabin_karp">Rabin-Karp</a></li>
+
+<li> <a href="/guillaume.rousseau/algo/texte/boyer_moore">Boyer-Moore</a></li>
+
 </ul>
 </div>
+
+<div class=category> 
+<h3><a href="/guillaume.rousseau/algo/autre">Autres</a></h3>
+<ul>
+<li> <a href="/guillaume.rousseau/algo/autre/rendu">Rendu de monnaie</a></li>
+</ul>
+</div>
+
+
+
+
 
 
 
