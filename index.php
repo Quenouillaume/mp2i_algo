587ff19f376d44af906baa40294e4f96090d6ebc
Guillaume <glm952@gmail.com>
2023-06-30 23:54:27 +0200

    added template

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Algorithmes MP2I</title>
+
+  <link rel="stylesheet" href="/guillaume.rousseau/algo/style.css">
+    
+</head>
+
+<body>
+
+<?php include("navigation.php"); ?>
+
+<main>
+
+<h1> Algorithmes de MP2I / MPI </h1>   
+
+Ce site répertorie les différents algorithmes au programme d'informatique
+de MP2I / MPI. Chaque algorithme est présenté par le biais d'une animation
+interactive montrant l'exécution du pseudo-code pas à pas. <br>
+
+<a href="https://github.com/quenouillaume/mp2i_algo">Ce projet est en cours de développement</a>  , il n'y a que quelques algorithmes
+pour l'instant, et le design du site est très primaire. 
+
+
+</main>
+</body>
+</html>
---
+++ template/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nom de l'algorithme</title>
+
+  <link rel="stylesheet" href="/guillaume.rousseau/algo/style.css">
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.js"></script>
+  <script src="/guillaume.rousseau/algo/algorithm.js"></script>
+  <script src="/guillaume.rousseau/algo/arrow.js"></script>
+  
+</head>
+
+<body>
+<?php include("/guillaume.rousseau/navigation.php"); ?>
+
+<main> 
+
+  <explanation> 
+  <h1>Nom de l'algorithme</h1>
+ Description courte
+
+
+  </explanation>
+
+
+  <div id=demo> 
+    <div id=p5Canvas>
+    </div>
+
+    <div id=code> 
+    <h3>Pseudo-code</h3>
+    Entrée:  <br>
+    Sortie:  <br>
+    <div id=pseudoCode></div>
+
+    <h3>Principe</h3>
+    Principe de fonctionnement.
+
+    <h3>Terminaison et complexité</h3>
+    Preuve de terminaison, analyse de complexité.
+
+    <h3>Correction</h3>
+    <div id=correction> 
+    Invariant de boucle, lien avec l'animation.
+    </div>
+
+
+    </div>
+  </div>
+
+</main>
+
+</body>
+
+</html>
